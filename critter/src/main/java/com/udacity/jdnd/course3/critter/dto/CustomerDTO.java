5041fd6bca00f09d01530d59f37bdc4dfc1a633f
TruongHungnb <57339328+TruongHungnb@users.noreply.github.com>
2023-08-25 23:10:22 +0700

    Update CustomerDTO.java

--- critter/src/main/java/com/udacity/jdnd/course3/critter/dto/CustomerDTO.java
+++ critter/src/main/java/com/udacity/jdnd/course3/critter/dto/CustomerDTO.java
@@ -2,7 +2,8 @@ package com.udacity.jdnd.course3.critter.dto;
 
 import lombok.Data;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Represents the form that customer request and response data takes. Does not map
@@ -10,49 +11,9 @@ import java.util.*;
  */
 @Data
 public class CustomerDTO {
-    private long id;
+    private Long id;
     private String name;
     private String phoneNumber;
     private String notes;
-    private List<Long> petIds;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public List<Long> getPetIds() {
-        return petIds;
-    }
-
-    public void setPetIds(List<Long> petIds) {
-        this.petIds = petIds;
-    }
+    private List<Long> petIds = new ArrayList<>();
 }
